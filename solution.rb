ecea4f0cbe22b46d4ded4ad90a77cc70c301123a
Fabio Andres Fonseca Arguelles <ffonseca@applusglobal.com>
2019-04-05 14:23:10 -0500

    fixing the h1 tag

--- solution.rb
+++ solution.rb
@@ -2,7 +2,7 @@ require "sinatra"
 
 get '/' do
     unless params[:nombre]
-        "<h1>Hola desconocido!<h1>"
+        "<h1>Hola desconocido!</h1>"
     else 
         "<h1>Hola #{params[:nombre].capitalize}!</h1>"
     end
