addf92b77e0fe44cbaf1cb4dc6dd166b86b7b31d
Fabio <fabiofonseca@Fabios-MacBook-Pro.local>
2019-04-05 22:34:38 -0500

    adding code for evalue nombre=empty

--- solution.rb
+++ solution.rb
@@ -4,6 +4,10 @@ get '/' do
     unless params[:nombre]
         "<h1>Hola desconocido!</h1>"
     else 
-        "<h1>Hola #{params[:nombre].capitalize}!</h1>"
+        if params[:nombre] == ""
+            "<h1>Hola desconocido!</h1>"
+        else
+            "<h1>Hola #{params[:nombre].capitalize}!</h1>"
+        end
     end
 end
